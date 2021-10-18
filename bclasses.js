8e77027ee7664b8164d6be47cb0f0c569adf596e
Gordon <gordon.tinyefuza@gmail.com>
2021-10-18 16:07:07 +0300

    Add remove, add, and getAllBooks methods

--- bclasses.js
+++ bclasses.js
@@ -16,3 +16,30 @@ class Book {
               <button class='remove' data-id='${this.id}'>Remove</button>
               <hr>`;
   }
+
+  static add(book) {
+    const bookItems = this.getAllBooks();
+    let id = 1;
+    if (bookItems.length > 0) {
+      id = bookItems[bookItems.length - 1].id + 1;
+    }
+    book.id = id;
+    bookItems.push(book);
+    saveToLocalStorage('bookItems', bookItems);
+  }
+
+  static remove(id) {
+    let bookItems = this.getAllBooks();
+    bookItems = bookItems.filter((b) => b.id !== Number(id));
+    saveToLocalStorage('bookItems', bookItems);
+  }
+
+  static getAllBooks() {
+    let bookItems = getFromLocalStorage('bookItems');
+    if (bookItems === null) {
+      bookItems = [];
+    }
+    return bookItems;
+  }
+}
+
