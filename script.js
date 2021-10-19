bd0a607a5d4992dfd4990b33dee8fa9f92a92dfe
sja_thedude <sja.affu765@gmail.com>
2021-10-19 14:51:53 +0300

    Updated

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-const saveToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
-const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
-
-class Book {
-  constructor(title, author, id = undefined) {
-    this.title = title;
-    this.author = author;
-    if (id !== undefined) {
-      this.id = id;
-    }
-  }
-
-  getCode() {
-    return ` <tr>
-              <td>${this.title} by ${this.author}</td>
-              <td><button class='remove' data-id='${this.id}'>Remove</td>
-              </tr>`;
-  }
-
-  static add(book) {
-    const bookItems = this.getAllBooks();
-    let id = 1;
-    if (bookItems.length > 0) {
-      id = bookItems[bookItems.length - 1].id + 1;
-    }
-    book.id = id;
-    bookItems.push(book);
-    saveToLocalStorage('bookItems', bookItems);
-  }
-
-  static remove(id) {
-    let bookItems = this.getAllBooks();
-    bookItems = bookItems.filter((b) => b.id !== Number(id));
-    saveToLocalStorage('bookItems', bookItems);
-  }
-
-  static getAllBooks() {
-    let bookItems = getFromLocalStorage('bookItems');
-    if (bookItems === null) {
-      bookItems = [];
-    }
-    return bookItems;
-  }
-}
-
 function displayBooks() {
   const bookItems = Book.getAllBooks();
 
@@ -78,3 +33,48 @@ document
     Book.add(book);
     displayBooks();
   });
+
+  const saveToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+  const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+  class Book {
+    constructor(title, author, id = undefined) {
+      this.title = title;
+      this.author = author;
+      if (id !== undefined) {
+        this.id = id;
+      }
+    }
+
+    getCode() {
+      return ` <tr>
+                <td>${this.title} by ${this.author}</td>
+                <td><button class='remove' data-id='${this.id}'>Remove</td>
+                </tr>`;
+    }
+
+    static add(book) {
+      const bookItems = this.getAllBooks();
+      let id = 1;
+      if (bookItems.length > 0) {
+        id = bookItems[bookItems.length - 1].id + 1;
+      }
+      book.id = id;
+      bookItems.push(book);
+      saveToLocalStorage('bookItems', bookItems);
+    }
+
+    static remove(id) {
+      let bookItems = this.getAllBooks();
+      bookItems = bookItems.filter((b) => b.id !== Number(id));
+      saveToLocalStorage('bookItems', bookItems);
+    }
+
+    static getAllBooks() {
+      let bookItems = getFromLocalStorage('bookItems');
+      if (bookItems === null) {
+        bookItems = [];
+      }
+      return bookItems;
+    }
+  }
