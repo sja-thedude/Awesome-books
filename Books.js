d2f49ab43d47a2a574cf02c48c147def0e62dec5
sja_thedude <sja.affu765@gmail.com>
2021-10-19 15:01:32 +0300

    Updated

--- Books.js
+++ Books.js
@@ -42,39 +42,3 @@ class Book {
     return bookItems;
   }
 }
-
-function displayBooks() {
-  const bookItems = Book.getAllBooks();
-
-  const booksCode = bookItems.map((book) => new Book(book.title, book.author, book.id).getCode());
-  document.getElementById('bitems').innerHTML = booksCode.join('');
-
-  const removeButtons = Array.from(document.querySelectorAll('.remove'));
-  removeButtons.forEach((removeButton) => {
-    removeButton.addEventListener('click', (event) => {
-      const id = event.target.getAttribute('data-id');
-      Book.remove(id);
-      displayBooks();
-    });
-  });
-}
-
-displayBooks();
-
-const titleInput = document.getElementById('btitle');
-const authorInput = document.getElementById('bauthor');
-document
-  .getElementById('bookslist')
-  .addEventListener('submit', (event) => {
-    event.preventDefault();
-    const title = titleInput.value.trim();
-    const author = authorInput.value.trim();
-
-    if (!title || !author) {
-      return;
-    }
-
-    const book = new Book(title, author);
-    Book.add(book);
-    displayBooks();
-  });
